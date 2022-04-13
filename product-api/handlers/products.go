b513b9433d734fd13ae521af17de76df3b31099b
Thong Hua <huavanthong14@gmail.com>
2022-04-13 20:25:13 +0700

    [Handler][Data] Step 4: Create method to encode data json from product, and implement handler for product

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,5 +1,12 @@
 package handlers
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/huavanthong/microservice-golang/product-api/data"
+)
+
 // Products is a http.Handler
 type Products struct {
 	l *log.Logger
@@ -10,3 +17,32 @@ func (p *Products) NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
+func (p *Products) ServerHTTP(rw http.ResponseWriter, r *http.Request) {
+
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProduct(rw, r)
+	}
+
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+
+}
+
+func (p *Products) getProduct(rw http.ResponseWriter, r *http.Request) {
+
+	// put logging
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
