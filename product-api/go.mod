d36c91c0b33ed53b9e1c5d8ae508d856aeb0af2f
Thong Hua <huavanthong14@gmail.com>
2022-04-13 20:25:47 +0700

    [Server] Step 5: create logger, and handler, setting configuration for server, running server

---
+++ product-api/go.mod
@@ -0,0 +1,8 @@
+module github.com/huavanthong/microservice-golang/product-api
+
+go 1.16
+
+require (
+	github.com/huavanthong/build-microservice-golang/microservice/product-api v0.0.0-20220412144916-5aa13a5c4323
+	github.com/nicholasjackson/env v0.6.0
+)
---
+++ product-api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/huavanthong/microservice-golang/product-api/handlers"
+	"github.com/nicholasjackson/env"
+)
+
+// declare environment server
+var bindAddress = env.String("BIND_ADDRESS", false, ":8080", "Bind address for the server")
+
+func main() {
+
+	env.Parse()
+
+	// create logger only for product-api
+	l := log.New(os.Stdout, "products-api", log.LstdFlags)
+
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new server mux and register the handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
+
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
