ab232e95b3f4f0c3bff01b7d56df9fb417fce259
Thong Hua <huavanthong14@gmail.com>
2022-04-16 16:40:53 +0700

    [Context][Validator] Update design for product to get logger and validator.

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,27 +10,49 @@ import (
 	"github.com/huavanthong/microservice-golang/product-api/data"
 )
 
-// Products is a http.Handler
+// KeyProduct is a key used for the Product object in the context
+type KeyProduct struct{}
+
+// Products handler for getting and updating products
 type Products struct {
 	l *log.Logger
+	v *data.Validation
 }
 
-// NewProducts creates a products handler with the given logger
-func NewProducts(l *log.Logger) *Products {
-	return &Products{l}
+// NewProducts returns a new products handler with the given logger
+func NewProducts(l *log.Logger, v *data.Validation) *Products {
+	return &Products{l, v}
 }
 
-// getProducts returns the products from the data store
-func (p *Products) ServerHTTP(rw http.ResponseWriter, r *http.Request) {
+// ErrInvalidProductPath is an error message when the product path is not valid
+var ErrInvalidProductPath = fmt.Errorf("Invalid Path, path should be /products/[id]")
 
-	// fetch the products from the datastore
-	lp := data.GetProducts()
+// GenericError is a generic error message returned by a server
+type GenericError struct {
+	Message string `json:"message"`
+}
 
-	// serialize the list to JSON
-	err := lp.ToJSON(rw)
+// ValidationError is a collection of validation error messages
+type ValidationError struct {
+	Messages []string `json:"messages"`
+}
+
+// getProductID returns the product ID from the URL
+// Panics if cannot convert the id into an integer
+// this should never happen as the router ensures that
+// this is a valid number
+func getProductID(r *http.Request) int {
+	// parse the product id from the url
+	vars := mux.Vars(r)
+
+	// convert the id into an integer and return
+	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		// should never happen
+		panic(err)
 	}
+
+	return id
 }
 
 // getProducts returns the products from the data store
