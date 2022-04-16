6ed35f6cc033cb116853e45c1c6643bf1114f3ef
Thong Hua <huavanthong14@gmail.com>
2022-04-16 14:10:49 +0700

    [Validate][Step 8] Implement function to validate structure Product.

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -4,16 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"regexp"
 	"time"
+
+	"github.com/go-playground/validator"
 )
 
+/************************ Define structure product ************************/
 // Product defines the structure for an API product
 type Product struct {
 	ID          int     `json:"id`
-	Name        string  `json:"name"`
+	Name        string  `json:"name" validate:"require"` // Validate-Step1: require to validate for this member.
 	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
+	Price       float32 `json:"price" validate:"gt=0"`
+	SKU         string  `json:"sku" validate:"require,sku"`
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
@@ -37,6 +41,30 @@ func (p *Products) ToJSON(w io.Writer) error {
 func (p *Product) FromJSON(r io.Reader) error {
 	e := json.NewDecoder(r)
 	return e.Decode(p)
+}
+
+/************************ Validation ************************/
+func (p *Product) Validate() error {
+	// Validate-Step2: create a object for this validator
+	validate := validator.New()
+
+	// Validate-Step3: register a customer validator function
+	validate.RegisterValidation("sku", validateSKU)
+
+	// Validate-Step4: validate structure product
+	return validate.Struct(p)
+}
+
+func validateSKU(fl validator.FieldLevel) bool {
+	// sku is of format abc-absd-dfsdf
+	re := regexp.MustCompile(`[a-z]+-[a-z]+-[a-z]+`)
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
 }
 
 /************************ Method for Product ************************/
