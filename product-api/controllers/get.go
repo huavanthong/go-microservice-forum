942e0ff423afd623b4a43380a8c3b805c2c4877b
Thong Hua <huavanthong14@gmail.com>
2022-04-16 16:41:59 +0700

    [RESTful][GET] Update controller API to get a list of products

---
+++ product-api/controllers/get.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/huavanthong/microservice-golang/product-api/data"
+)
+
+func (p *Products) ListAll(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("[DEBUG] get all records")
+
+	prods := data.GetProducts()
+
+	err := data.ToJSON(prods, rw)
+	if err != nil {
+		// we should never be here but log the error just incase
+		p.l.Println("[ERROR] serializing product", err)
+	}
+}
--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -55,20 +55,6 @@ func getProductID(r *http.Request) int {
 	return id
 }
 
-// getProducts returns the products from the data store
-func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle GET Products")
-
-	// fetch the products from the datastore
-	lp := data.GetProducts()
-
-	// serialize the list to JSON
-	err := lp.ToJSON(rw)
-	if err != nil {
-		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
-	}
-}
-
 func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Product")
 
