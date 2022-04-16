1e17d7411e49dedcb152a690c7f79dd839e7b9a0
Thong Hua <huavanthong14@gmail.com>
2022-04-16 16:33:49 +0700

    [Middleware] Move middleware to another file

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -76,25 +76,3 @@ func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-
-type KeyProduct struct{}
-
-func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		prod := data.Product{}
-
-		err := prod.FromJSON(r.Body)
-		if err != nil {
-			p.l.Println("[ERROR] deserializing product", err)
-			http.Error(rw, "Error reading product", http.StatusBadRequest)
-			return
-		}
-
-		// add the product to the context
-		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
-		r = r.WithContext(ctx)
-
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(rw, r)
-	})
-}
