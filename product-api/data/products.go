8f55ee9519df7062dcd813bd2b0bd1ef8d393bbb
Thong Hua <huavanthong14@gmail.com>
2022-04-16 16:50:58 +0700

    [Data] Update all methods for this products

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -22,17 +22,32 @@ type Product struct {
 type Products []*Product
 
 /************************ Method for Product ************************/
+/************ GET ************/
 // GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
 }
 
+// GetProductByID returns a single product which matches the id from the
+// database.
+// If a product is not found this function returns a ProductNotFound error
+func GetProductByID(id int) (*Product, error) {
+	i := findIndexByProductID(id)
+	if id == -1 {
+		return nil, ErrProductNotFound
+	}
+
+	return productList[i], nil
+}
+
+/************ POST ************/
 // AddProduct addies a product to list
 func AddProduct(p *Product) {
 	p.ID = getNextID()
 	productList = append(productList, p)
 }
 
+/************ PUT ************/
 // UpdateProduct updates info to product
 func UpdateProduct(id int, p *Product) error {
 
@@ -45,6 +60,19 @@ func UpdateProduct(id int, p *Product) error {
 	// update info product
 	p.ID = id
 	productList[pos] = p
+
+	return nil
+}
+
+/************ DELETE ************/
+// DeleteProduct deletes a product from the database
+func DeleteProduct(id int) error {
+	i := findIndexByProductID(id)
+	if i == -1 {
+		return ErrProductNotFound
+	}
+
+	productList = append(productList[:i], productList[i+1])
 
 	return nil
 }
@@ -66,6 +94,18 @@ func findProduct(id int) (*Product, int, error) {
 		}
 	}
 	return nil, -1, ErrProductNotFound
+}
+
+// findIndex finds the index of a product in the database
+// returns -1 when no product can be found
+func findIndexByProductID(id int) int {
+	for i, p := range productList {
+		if p.ID == id {
+			return i
+		}
+	}
+
+	return -1
 }
 
 /************************ Storage Product ************************/
