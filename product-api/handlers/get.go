5ec47e5ef808c7df7cdaebcef3189c933503cd48
Thong Hua <huavanthong14@gmail.com>
2022-05-08 14:36:21 +0700

    [Product-API][Logger] Update design after using hclog package.

--- product-api/handlers/get.go
+++ product-api/handlers/get.go
@@ -15,14 +15,14 @@ import (
 
 // ListAll handles GET requests and returns all current products
 func (p *Products) ListAll(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("[DEBUG] get all records")
+	p.l.Debug("Get all records")
 
 	prods := data.GetProducts()
 
 	err := data.ToJSON(prods, rw)
 	if err != nil {
 		// we should never be here but log the error just incase
-		p.l.Println("[ERROR] serializing product", err)
+		p.l.Error("Unable to serializing product", "error", err)
 	}
 }
 
@@ -36,7 +36,7 @@ func (p *Products) ListAll(rw http.ResponseWriter, r *http.Request) {
 func (p *Products) ListSingle(rw http.ResponseWriter, r *http.Request) {
 	id := getProductID(r)
 
-	p.l.Println("[DEBUG] get record id", id)
+	p.l.Debug("Get record", "id", id)
 
 	// find product by id
 	prod, err := data.GetProductByID(id)
