5c3e550a3498c87883c141638c4a82c24d158a56
hvthong <huavanthong14@gmail.com>
2022-05-11 14:49:40 +0700

    [Controller] Update Restful for querying id from request.

--- user-api/controllers/user.go
+++ user-api/controllers/user.go
@@ -32,9 +32,14 @@ func (u *User) AddUser(ctx *gin.Context) {
 
 // ListUsers get all users exist in DB
 func (u *User) ListUsers(ctx *gin.Context) {
+
+	// array of users
 	var users []models.User
+
+	// get all users
 	users, err := u.userDAO.GetAll()
 
+	// write response
 	if err == nil {
 		ctx.JSON(http.StatusOK, users)
 	} else {
@@ -46,12 +51,13 @@ func (u *User) ListUsers(ctx *gin.Context) {
 
 func (u *User) GetUserByID(ctx *gin.Context) {
 
-	// get id from context
+	// filter parameter id from context
 	id := ctx.Params.ByName("id")
 
 	// find user by id
 	user, err := u.userDAO.GetByID(id)
 
+	// write response
 	if err == nil {
 		ctx.JSON(http.StatusOK, user)
 	} else {
@@ -62,6 +68,19 @@ func (u *User) GetUserByID(ctx *gin.Context) {
 
 func (u *User) GetUserByParams(ctx *gin.Context) {
 
+	// filter parameter id from request on context
+	id := ctx.Request.URL.Query()["id"][0]
+
+	// find user by id
+	user, err := u.userDAO.GetByID(id)
+
+	// write response
+	if err == nil {
+		ctx.JSON(http.StatusOK, user)
+	} else {
+		ctx.JSON(http.StatusInternalServerError, models.Error{common.StatusCodeUnknown, err.Error()})
+		fmt.Errorf("[ERROR]: ", err)
+	}
 }
 
 func (u *User) DeleteUserByID(ctx *gin.Context) {
