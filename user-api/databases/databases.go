a2e39223c5bb8c8f70ad0a4f23e1a1632113e0c3
hvthong <huavanthong14@gmail.com>
2022-05-11 16:08:55 +0700

    [DesignPattern][Singleton] Implement singleton pattern for our DB.

--- user-api/databases/databases.go
+++ user-api/databases/databases.go
@@ -5,7 +5,45 @@
  */
 package databases
 
+import (
+	"fmt"
+	"sync"
+)
+
 // Database shares global database instance
 var (
 	Database MongoDB
 )
+
+/*********************************************************************
+Design Pattern: Singleton
+
+Feature:
+	+ Get instance DB for MongoDB
+
+Purpose:
+	+ to get only a instance for DB,
+	+ and other clients can't access to DB if instance is exist
+
+Refer: https://golangbyexample.com/singleton-design-pattern-go/
+*********************************************************************/
+var singleInstance *MongoDB
+
+var lock = &sync.Mutex{}
+
+func getInstanceDB() *MongoDB {
+	if singleInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleInstance == nil {
+			fmt.Println("Creting Single Instance Now")
+			singleInstance = &Database
+		} else {
+			fmt.Println("Single Instance already created-1")
+		}
+	} else {
+		fmt.Println("Single Instance already created-2")
+	}
+
+	return singleInstance
+}
