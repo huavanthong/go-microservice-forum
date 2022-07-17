23db50b674299d76832da9d435ececceb06689c1
Thong Hua <huavanthong14@gmail.com>
2022-07-17 09:43:40 +0700

    [Module] Update module for project

--- email-microservice/main.go
+++ email-microservice/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "0.0.0.0:8080"
+)
+
+func main() {
+	// create connection to grpc email service
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grcp.WithBlock())
+	if err != nil {
+		log.Fatal("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	// client := pb.
+}
