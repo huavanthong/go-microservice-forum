44876d0cdbe11b0f45e2f79cc0a0c8c85a78b12f
hvthong <huavanthong14@gmail.com>
2022-08-25 11:45:43 +0700

    [Models] Update category type.

--- product-api-v3/models/category.model.go
+++ product-api-v3/models/category.model.go
@@ -1,15 +1,21 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 /************************ Define structure product ************************/
 type Category struct {
-	ID          primitive.ObjectID `bson:"_id" json:"id" example:"5bbdadf782ebac06a695a8e7"`
-	Name        string             `json:"name" bson:"name"`
-	Description string             `json:"description" bson:"description"`
-	CreatedAt   string             `json:"created_at" bson:"created_at"`
-	UpdatedAt   string             `json:"updated_at" bson:"updated_at"`
-	DeleteAt    string             `json:"deleted_at" bson:"deleted_at"`
+	ID            int           `bson:"_id" json:"id" example:"001"`
+	Name          string        `json:"name" bson:"name"`
+	SubCategories []SubCategory `json:"subcategory" bson:"subcategory"`
+	Description   string        `json:"description" bson:"description"`
+	CreatedAt     string        `json:"created_at" bson:"created_at"`
+	UpdatedAt     string        `json:"updated_at" bson:"updated_at"`
+	DeleteAt      string        `json:"deleted_at" bson:"deleted_at"`
+}
+
+type SubCategory struct {
+	CategoryType int    `bson:"categorytype" json:"categorytype" example:"001"`
+	Name         string `json:"name" bson:"name"`
+	Description  string `json:"description" bson:"description"`
+	CreatedAt    string `json:"created_at" bson:"created_at"`
+	UpdatedAt    string `json:"updated_at" bson:"updated_at"`
+	DeleteAt     string `json:"deleted_at" bson:"deleted_at"`
 }
