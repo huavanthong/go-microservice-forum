4d965165ad01d67d70488d6bd1539f99f5b75d4b
htv3hc <thong.huavan@vn.bosch.com>
2023-10-24 13:16:14 +0700

    Final test sed command to replace string.

--- update_repos.sh
+++ update_repos.sh
@@ -5,10 +5,13 @@
 # Đường dẫn đến tệp repository.conf
 CONFIG_FILE="./config/repositories.conf"
 
+REPOSITORY=robotframework-testsuitesmanagement
+PULL_MERGE_BRANCH=htv3hc/feat/trigger-other-workflow
+
 if [ -n "$REPOSITORY"  ] && [ -n "$PULL_MERGE_BRANCH" ]; then 
     echo "Exist to update"
      # Thay đổi giá trị trong tệp repository.conf
-    sed -i "s/$REPOSITORY=\(.*\)/${REPOSITORY}=$PULL_MERGE_BRANCH/" "$CONFIG_FILE"
+    sed -i "s,$REPOSITORY=.*,$REPOSITORY=$PULL_MERGE_BRANCH," $CONFIG_FILE
     echo "Updated $REPOSITORY in $CONFIG_FILE with value $PULL_MERGE_BRANCH"
 fi
 
