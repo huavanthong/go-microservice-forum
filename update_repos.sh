780beb6d24999548be6653d740b960a0e7aa5240
htv3hc <thong.huavan@vn.bosch.com>
2023-11-17 10:12:01 +0700

    Update code

--- update_repos.sh
+++ update_repos.sh
@@ -16,13 +16,13 @@ function greenmsg(){
    echo -e "${COL_GREEN}> $1.${COL_RESET}"   
 }
 
-# Khai báo giá trị mới cho REPOSITORY và PULL_MERGE_BRANCH
+# Khai báo giá trị mới cho REPOSITORY và PULL_REQUEST_BRANCH
 
 # Đường dẫn đến tệp repository.conf
-CONFIG_FILE="./config/repositories.conf"
+config_file="./config/repositories.conf"
 
 echo "Check repos: $REPOSITORY"
-echo "Check branch: $PULL_MERGE_BRANCH"
+echo "Check branch: $PULL_REQUEST_BRANCH"
 echo "Check version: $TEST_VERSION"
 echo "Check date: $TEST_DATE"
 echo "Check at env - help me: $HELP_ME"
@@ -39,15 +39,15 @@ echo "Test Version: $test_version"
 echo "Test Date: $test_date"
 
 
-if [ -n "$REPOSITORY"  ] && [ -n "$PULL_MERGE_BRANCH" ]; then 
-	echo -e "${COL_GREEN}#          Trigger AIO from repository: $REPOSITORY; branch: $PULL_MERGE_BRANCH    #${COL_RESET}"
+if [ -n "$REPOSITORY"  ] && [ -n "$PULL_REQUEST_BRANCH" ]; then 
+	echo -e "${COL_GREEN}#          Trigger AIO from repository: $REPOSITORY; branch: $PULL_REQUEST_BRANCH    #${COL_RESET}"
 
-	grep -q $REPOSITORY $CONFIG_FILE
+	grep -q $REPOSITORY $config_file
 	if [ $? -ne 0 ]; then
-    	errormsg "Repository received from request not found in '$CONFIG_FILE'"
+    	errormsg "Repository received from request not found in '$config_file'"
 	else
-		sed -i "s,$REPOSITORY=.*,$REPOSITORY=$PULL_MERGE_BRANCH," $CONFIG_FILE
-		echo -e "${COL_GREEN}#          Updated $REPOSITORY in $CONFIG_FILE with value $PULL_MERGE_BRANCH"
+		sed -i "s,$REPOSITORY=.*,$REPOSITORY=$PULL_REQUEST_BRANCH," $config_file
+		echo -e "${COL_GREEN}#          Updated $REPOSITORY in $config_file with value $PULL_REQUEST_BRANCH"
 	fi
 fi
 
@@ -56,10 +56,10 @@ fi
 # sed -i 's/microservice-golang=\(.*\)/microservice-golang=test/' repositories.conf
 
 # # Kiểm tra xem tệp tồn tại trước khi thay đổi
-# if [ -f "$CONFIG_FILE" ]; then
+# if [ -f "$config_file" ]; then
 #     # Thay đổi giá trị trong tệp repository.conf
-#     sed -i "s/$REPOSITORY=\(.*\)/${REPOSITORY}=$PULL_MERGE_BRANCH/" "$CONFIG_FILE"
-#     echo "Updated $REPOSITORY in $CONFIG_FILE with value $PULL_MERGE_BRANCH"
+#     sed -i "s/$REPOSITORY=\(.*\)/${REPOSITORY}=$PULL_REQUEST_BRANCH/" "$config_file"
+#     echo "Updated $REPOSITORY in $config_file with value $PULL_REQUEST_BRANCH"
 # else
-#     echo "$CONFIG_FILE does not exist."
+#     echo "$config_file does not exist."
 # fi
