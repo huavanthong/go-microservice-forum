5bfca5736de7b5125d45cb64999d7b321150da16
htv3hc <thong.huavan@vn.bosch.com>
2024-03-14 14:58:26 +0700

    Ignore error

--- requirements_windows.ps1
+++ requirements_windows.ps1
@@ -31,6 +31,6 @@ $extra_packages = $texlive_packages -join ','
 $TEXLIVE_DIR = "D:/texlive/aio"
 
 # choco install texlive --version=2022.20221202 --params "'/collections:pictures,latex,latexextra,latexrecommended'" --execution-timeout 5400
-choco install texlive --version=2022.20221202 --params "'/collections:pictures,latex /InstallationPath:${TEXLIVE_DIR} /extraPackages:${extra_packages}'"
+choco install texlive --version=2022.20221202 --params "'/collections:pictures,latex /InstallationPath:${TEXLIVE_DIR} /extraPackages:${extra_packages}'" -ErrorAction SilentlyContinue
 
 $env:GENDOC_LATEXPATH = "${TEXLIVE_DIR}/bin/windows"
